419b2abc1d9a34178d2ca2e0a81cbb686947f37b
Hackbright Student <no-reply@hackbrightacademy.com>
2018-10-11 15:54:04 -0700

    did most of the remote studies

--- melons.py
+++ melons.py
@@ -1,20 +1,50 @@
 """Classes for melon orders."""
+import random
+from datetime import datetime
+
+
+class TooManyMelonsError(ValueError):
+	pass
+	# def __init__(self, expression):
+	# 	self.expression = expression
+	# 	self.message = "That's too many melons!"
 
 class AbstractMelonOrder():
 	"""Generic Melon Order"""
 
 	def __init__(self, species, qty):
 		"""Initialize melon order attributes."""
-
+		if qty > 100:
+			raise TooManyMelonsError
 		self.species = species
 		self.qty = qty
 		self.shipped = False
 		self.order_type = ""
-		self.tax = None
+		self.tax = 0
+		self.order_time = datetime.now()
+		# self.order_time = datetime(2018, 10, 13, 8,30,0,0)
+
+	def get_base_price(self):
+		"""Determine the base price of melon"""
+		
+		baseprice = random.randint(5,10)
+		# print("baseprice from get_base_price method is originally {}".format(baseprice))
+		
+		#check for weekday and check for time
+		#if mon-fri and in the rush hour time, then baseprice is 4+baseprice
+		weekday = self.order_time.weekday()
+		hour = self.order_time.time().hour
+		if weekday >=0 and weekday <5:
+			if hour >=8 and hour <=11:
+				baseprice += 4
+				# print("baseprice from get_base_price method is now changed to {}".format(baseprice))
+
+		return baseprice
 
 	def get_total(self):
 		"""Calculate price, including tax."""
-		base_price = 5
+		base_price = self.get_base_price()
+		# print("base_price from the get total method is {}".format(base_price))
 		
 		if self.species == "christmas":
 			base_price = 1.5*base_price
@@ -61,3 +91,15 @@ class InternationalMelonOrder(AbstractMelonOrder):
 		"""Return the country code."""
 
 		return self.country_code
+
+
+class GovernmentMelonOrder(AbstractMelonOrder):
+
+	def __init__(self, species, qty):
+
+		super().__init__(species,qty)
+		self.tax = 0
+		self.passed_inspection = False
+
+	def mark_inspection(self, passed):
+		self.passed_inspection = passed
